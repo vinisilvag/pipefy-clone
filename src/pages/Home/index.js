c50322f8527b1a1e146f2fdb7ead5fbc65885566
Vinicius Gomes <gowesvini@gmail.com>
2020-11-02 22:05:53 -0300

    Initial commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Header from '../../components/Header';
+import Board from '../../components/Board';
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+      <Board />
+    </>
+  );
+};
+
+export default Home;
